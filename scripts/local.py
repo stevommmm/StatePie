8a5f89d56a6c72d80a510c193a9b80170acd445e
stevommmm <git@c45y.com>
2015-07-29 10:32:37 +1000

    remove pointless 1s load check

--- scripts/local.py
+++ scripts/local.py
@@ -4,12 +4,6 @@ import socket
 import json
 
 print json.dumps({
-	'uid': socket.gethostname() + "_cpu_perc_1s",
-	'percent': round(psutil.cpu_percent(interval=1), 1),
-	'description': '1s CPU usage for ' + socket.gethostname(),
-})
-
-print json.dumps({
 	'uid': socket.gethostname() + "_cpu_perc_1m",
 	'percent': round(psutil.cpu_percent(interval=60), 1),
 	'description': '1m CPU usage for ' + socket.gethostname(),
