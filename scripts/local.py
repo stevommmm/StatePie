b0d9857267c098af2c941d64e352e6297f9490e2
stevommmm <git@c45y.com>
2015-07-30 11:08:59 +1000

    wrong keyword

--- scripts/local.py
+++ scripts/local.py
@@ -8,7 +8,7 @@ print json.dumps({
 	'uid': socket.gethostname() + "_cpu_perc_1m",
 	'percent': cpu_per,
 	'description': '1m CPU usage for ' + socket.gethostname(),
-	'level': 'warn' if cpu_per > 90 else 'normal'
+	'state': 'warn' if cpu_per > 90 else 'normal'
 })
 
 vmem_per = round(psutil.virtual_memory().percent, 1)
@@ -16,5 +16,5 @@ print json.dumps({
 	'uid': socket.gethostname() + "_mem_perc",
 	'percent': vmem_per,
 	'description': 'Memory usage for ' + socket.gethostname(),
-	'level': 'warn' if vmem_per > 90 else 'normal'
+	'state': 'warn' if vmem_per > 90 else 'normal'
 })
